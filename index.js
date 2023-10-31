e1a54ea2c2fb5fda94db704dfb369366169bb706
unknown <nguyenquethi186@gmail.com>
2023-10-30 23:48:44 -0400

    second update

--- index.js
+++ index.js
@@ -11,7 +11,28 @@ document.getElementById('btnTinhTongSoDuong').addEventListener('click', function
    for (var i = 0; i < arrSoN.length; i++) {
        if (arrSoN[i] > 0) {
         sum += arrSoN[i];
+        count++;
        }
    }
    alert (sum);
+});
+
+document.getElementById('btnSoNhoNhat').addEventListener('click', function() {
+    for (var i = 0; i < arrSoN.length; i++) {
+        var min = arrSoN[0];
+        if (arrSoN[i] < min) {
+            min = arrSoN[i];
+        }
+    }
+    alert(`Số nhỏ nhất trong mảng ${min}`);
+});
+
+document.getElementById('btnSoDuongNhoNhat').addEventListener('click', function() {
+    for (var i = 0; i < arrSoN.length; i++) {
+        var minPos = arrSoN[0];
+        if (arrSoN[i] > 0 && arrSoN[i] < minPos){
+            minPos = arrSoN[i];
+        }
+    }
+    alert(`Số dương nhỏ nhất trong mảng ${minPos}`);
 });
